c6490ed304424e6c3fd5fe8d40847de958e22893
Baptiste Cottier <baptiste.cottier@icloud.com>
2025-03-15 23:47:37 +0100

    Adding pi day - year 2024

---
+++ pi day/README.md
@@ -0,0 +1,11 @@
+# Pi Day Coding Quest
+
+This repository contains my solutions for the [Pi Day](https://ivanr3d.com/projects/pi/) annual coding quest.
+
+||  |
+|:------:|-----------|
+| **2024** | **2025** |
+| [π](https://github.com/baptistecottier/other_puzzles/tree/main/pi-day/events/year_2024/year_2024.py) |  |
+
+
+
---
+++ pi day/events/year_2024/year_2024.py
@@ -0,0 +1,39 @@
+"""
+Solver for 2024 - Pi Day Coding Quest
+"""
+
+def preprocessing(puzzle_input):
+    """
+    Preprocess the puzzle input by converting it to lowercase.
+    """
+    return puzzle_input.lower()
+
+
+def solver(message):
+    """
+    Solves a puzzle by processing a message using Pi digits.
+    
+    Shifts each character in the message backwards by corresponding Pi digits,
+    then calculates a code based on occurrences of digit words in the message.
+    
+    Args:
+        message (str): The input message to process
+        
+    Yields:
+        int: The calculated code value
+    """
+    alphabet   = "abcdefghijklmnopqrstuvwxyz"
+    pi_digits  = list(map(int, "3141592653589793"))
+    digits_str = ["two", "three", "four",
+                  "five", "six", "seven",
+                  "eight", "nine", "ten"]
+
+    for n, c in enumerate(message):
+        if c not in " -!',.":
+            message += alphabet[(alphabet.index(c) - pi_digits[n % 16]) % 26]
+
+    code = 1
+    for i, w in enumerate(digits_str, 2):
+        if w in message:
+            code *= pow(i, message.count(w))
+    yield code
