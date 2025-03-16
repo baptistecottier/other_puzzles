f949c6287b61f721e89f89e0d0bac879d990b3c3
Baptiste Cottier <baptiste.cottier@icloud.com>
2025-03-16 01:13:49 +0100

    Remove test input handling for Pi Day 2024 and update output format

--- pi day/pi_day.py
+++ pi day/pi_day.py
@@ -10,11 +10,6 @@ if __name__ == '__main__':
     sys.path.append(INPUT_PATH)
     module = __import__(f"year_{year}")
 
-    with open(f"{INPUT_PATH}/test_{year}.txt", encoding = "utf-8") as file:
-        test_input = file.read()
-        test_input, expected_test_answers = test_input.rsplit('\n', 1)
-        expected_test_answers = expected_test_answers.splitlines()
-
     with open(f"{INPUT_PATH}/user_{year}.txt", encoding = "utf-8") as file:
         user_input = file.read()
 
@@ -26,16 +21,5 @@ if __name__ == '__main__':
             return x
 
     computed_user_answers = module.solver(pp(user_input))
-    print("User answer:", next(computed_user_answers))
-
-    # computed_test_answers = module.solver(pp(test_input))
-    # for expected_test_answer in expected_test_answers:
-    #     computed_test_answer = next(computed_test_answers)
-    #     if  str(computed_test_answer) == expected_test_answer:
-    #         print("Test passed ✅")
-    #         computed_user_answers = module.solver(pp(user_input))
-    #         print("User answer:", next(computed_user_answers))
-    #     else:
-    #         print("Test failed ❌. "
-    #             f"Your answer: {computed_test_answer}. "
-    #             f"Expected answer: {expected_test_answer}")
+    for part, answer in enumerate(computed_user_answers, 1):
+        print(f"Part {part}: {answer}")
